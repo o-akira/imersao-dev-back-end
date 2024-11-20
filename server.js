db329c2438269fa3733eae6bd34f342abeba6769
o-akira <h.hiraga2002@gmail.com>
2024-11-20 10:57:39 -0300

    Aula 3

--- server.js
+++ server.js
@@ -1,56 +1,9 @@
 import express from "express";
-
-const posts = [
-  {
-    id: 1,
-    descricao: "Uma foto teste",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Gato fofo dormindo",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 3,
-    descricao: "Paisagem montanhosa",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 4,
-    descricao: "Cachorro brincando",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 5,
-    descricao: "Comida deliciosa",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 6,
-    descricao: "Cidade à noite",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
   console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-function buscaPostPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscaPostPorID(req.params.id);
-  res.status(200).json(posts[index]);
 });
